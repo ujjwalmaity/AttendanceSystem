2962241fde5b3a5afd65cfeb9f47b1092bc91804
ujjwal <ujjwalmaity98@gmail.com>
2019-10-11 04:53:15 +0530

    Take a photo with a camera app And retrieves this image and displays it in an ImageView.

--- app/src/main/java/dev/ujjwal/attendancesystem/AttendanceActivity.java
+++ app/src/main/java/dev/ujjwal/attendancesystem/AttendanceActivity.java
@@ -2,13 +2,58 @@ package dev.ujjwal.attendancesystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class AttendanceActivity extends AppCompatActivity {
+public class AttendanceActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView imageView;
+    Button button;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
+
+        init();
+
+        button.setOnClickListener(this);
+    }
+
+    private void init() {
+        imageView = findViewById(R.id.attendance_image);
+        button = findViewById(R.id.attendance_capture_image);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.attendance_capture_image:
+                dispatchTakePictureIntent();
+                break;
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
